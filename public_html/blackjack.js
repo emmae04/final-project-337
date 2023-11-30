38ac8bd6dc78e510a515217a39d92190b1cb2082
emmae04 <119893663+emmae04@users.noreply.github.com>
2023-11-30 15:56:48 -0700

    Update 2

--- public_html/blackjack.js
+++ public_html/blackjack.js
@@ -31,129 +31,344 @@
 var suits = ["S", "D", "C", "H"];
 var rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-function getDeck()
-{
-	let deck = [];
-	for(let i = 0; i < suits.length; i++)
-	{		for(let x = 0; x < values.length; x++)
-		{
-            let card = [rank[x] + suits[i]];
-			deck.push(card);
-		}
-	}
-	return deck
-}
-
-
-fetch("/")
-.then((res) => {
-	console.log(res);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-const deck = getDeck();
-const currentCards = []
-
-function initiateGame() {
-	var countdown = document.getElementById('countdown');
-
-	// while (!p1Stick) {
-	// 	secondsleft = 10;
-	// 	countdownInterval = setInterval(function() {
-	// 		seconds--;
-	// 		countdown.textContent = secondsleft;
-	// 	if (secondsleft <= 0) {
-	// 		clearInterval(countdownInterval);
-	// 		countdown.textContent = 0;
-	// 		}
-	// 	}, 1000);
-			
-	// }
-
-	
-}
-
-const p1Stick = false;
-const p2Stick = false;
-
-var initValP1 = displayP1Cards(player1Cards);
-var initValP2 = displayP2Cards(player2cards);
-
-var player1Cards = pickCards();
-var player2cards = pickCards();
-
-initiateGame();
-
-function pickAnother() {
-	card = chooseCard()
-	return card
-	
-}
-
-function stick() {
-	p1Stick = true
-	
-}
-
-
-function displayP1Cards(player1Cards) {
-	var card1Suit = player1Cards[0].split(" ")[0];
-	var card1Rank = player1Cards[0].split(" ")[1];
-
-	cardCont=document.getElementById("P1Cards");
-
-	var card1 = document.createElement('img');
-	card1.src = "PNG/" + card1Suit + "-" + card1Rank + ".png";
-	cardCont.appendChild(card1);
-
-	var card2Suit = player1Cards[1].split(" ")[0];
-	var card2Rank = player1Cards[1].split(" ")[1];
-
-	var card2 = document.createElement('img');
-	card2.src = "PNG/" + card2Suit + "-" + card2Rank + ".png";
-	cardCont.appendChild(card2);
-
-	return card1Rank + card2Rank
-
-}
-
-function displayP2Cards(player2Cards) {
-	cardCont=document.getElementById("P2Cards");
-
-	var card1 = document.createElement('img');
-	card1.src = "playing-card.png";
-	cardCont.appendChild(card1);
-
-	var card2 = document.createElement('img');
-	card2.src = "playing-card.png"
-	cardCont.appendChild(card2);
-
-	return player2Cards[0].split(" ")[1] + player2Cards[1].split(" ")[1];
-}
-
-function pickCards() {
-	var card1 = chooseCard();
-	var card2 = chooseCard();
-	return card1, card2
-}
-
 function chooseCard() {
+	
 	var card = deck[Math.floor(Math.random() * 52)];
 	while (!deck.includes(card)) {
 		card = deck[Math.floor(Math.random() * 52)];
 	}
 	currentCards.push(card);
 	return card
+}
+//create a function to initialize values of game
+// deck, current cards
+var numRounds = 0;
+var P1Wins = 0;
+var P2Wins = 0;
+const deck = []
+var currentCards = []
+var player1Cards = []
+var player2Cards = []
+const cardCont1=document.getElementById("P1Cards");
+const cardCont2=document.getElementById("P2Cards");
+const drawAnotherButton = document.getElementById("drawAnother");
+const stickButton = document.getElementById("stick");
+const noPlayButton = document.getElementById("dont-play-again");
+const playAgain = document.getElementById("play-again");
+const txtbox = document.getElementById("textbox");	
+const startButton = document.getElementById("start");	
+const secondsLeft = document.getElementById("secondsLeft");
+var player1Sticks = false;
+var player2Sticks = false;
+var p1Moved = false;
+var intID1 = 0;
+var intID2 = 0;
+var intID3 = 0;
+var player1CardVal = 0;
+var player2CardVal = 0;
+var gameOver = false;
+
+function initValues() {
+	drawAnotherButton.disabled = false;
+	stickButton.disabled = false;
+	player1Cards = [];
+	player2Cards = [];
+	player1CardVal = 0;
+	player2CardVal = 0;
+	currentCards = [];
+	p1Moved = false;
+	player1Sticks = false;
+	player2Sticks = false;
+	txtbox.style.display = "None";
+	noPlayButton.style.display = "None";
+	playAgain.style.display = "None";
+	if (numRounds >= 1) {
+		cardCont2.innerHTML = "";
+		cardCont1.innerHTML = "";
+	}
+	for(let i = 0; i < suits.length; i++)
+	{		for(let x = 0; x < rank.length; x++)
+		{
+            let card = [rank[x] + " " + suits[i]];
+			deck.push(card);
+		}
+	}
+}
+
+
+// create a function to choose players first cards
+function chooseFirstCards() {
+	player1Cards.push(chooseCard());
+	player1Cards.push(chooseCard());
+	player2Cards.push(chooseCard());
+	player2Cards.push(chooseCard());
+}
+
+
+function startGame() {
+	initValues();
+	intID1 = setInterval(countDown, 1000);
+	intID2 = setInterval(playComputersTurn, 1000);
+	intID3 = setInterval(isGameOver, 500);
+	startButton.style.display = "None";
+	chooseFirstCards();
+
+	displayP1Cards();
+	displayP2Cards();
+	
+}
+
+function playComputersTurn() {
+	if (p1Moved || player1Sticks) {
+		secondsLeft.innerText = "10"
+		computerDraws()
+		p1Moved = false;
+
+	}
+}
+
+function isGameOver() {
+	if (player1Sticks && player2Sticks) {
+		stopRound()
+	}
+	if (player1CardVal > 21 || player2CardVal > 21) {
+		stopRound()
+	}
+}
+
+function computerDraws() {
+	if (tDontD(player2CardVal)) {
+		pickAnother(2)
+	}
+	else {
+		console.log("here")
+		player2Sticks = true;
+	}
+
+}
+
+function tDontD(player2CardVal) {
+	if (player2CardVal >= 21) {
+		return false;
+	}
+	random = Math.floor(Math.random() * 3)
+	return random != 1
+}
+
+function pickAnother(player) {
+	let card = chooseCard()
+	var anotherCard = document.createElement('img');
+	anotherCard.style.width = "100px";
+	anotherCard.style.marginRight = "25px";
+	anotherCard.style.borderRadius = "15px";
+	if (player == 2) {
+		anotherCard.src = "playing-card.png";
+		cardCont2.appendChild(anotherCard);
+		secondsLeft.innerText = 10;
+		player2Cards.push(card);
+		addCardVal(2, card);
+	}
+	else {
+		var cardSrc = String(card).split(" ")[0] + String(card).split(" ")[1];
+		anotherCard.src = "cards/" + cardSrc + ".png"
+		cardCont1.appendChild(anotherCard);
+		secondsLeft.innerText = 10;
+		p1Moved = true;
+		player1Cards.push(card);
+		addCardVal(1, card)
+	}
+	currentCards.push(card);
+
+
+}
+
+function stick() {
+	player1Sticks = true;
+	p1Moved = true;
+	drawAnotherButton.disabled = true;
+	stickButton.disabled = true;
+
+}
+
+
+function countDown() {
+	var countdown = ["10", "9", "8", "7", "6", "4", "5", "3",
+	"2", "1", "0"];
+	currVal = secondsLeft.innerText;
+	currIndex = countdown.indexOf(currVal);
+	if (currVal == 0) {
+		if (!p1Moved) {
+			stopRound()
+			secondsLeft.innerText = 0;
+		}
+		else {
+			secondsLeft.innerText = 10;
+		}
+	}
+	else if (currVal >= 1) {
+		secondsLeft.innerText = countdown[currIndex+1];
+	}
+
+}
+
+function stopRound() {
+	winner = whosTheWinner()
+	if (whosTheWinner() == 1) {
+		P1Wins += 1
+		winnerMsg = "You win! "
+	}
+	else if (whosTheWinner() == 2) {
+		P2Wins += 1
+		winnerMsg = "You lost. "
+	}
+	else {
+		winnerMsg = "Tie. "
+	}
+	winnerMsg += "You : " + String(player1CardVal) + " Player 2 : " + String(player2CardVal);
+
+	txtbox.style.display = "inline";
+	txtbox.innerText = winnerMsg;
+	clearInterval(intID1);
+	clearInterval(intID2);
+	displayCurrScore();
+	clearInterval(intID3)
+	playAgain.style.display = 'inline';
+	noPlayButton.style.display = 'inline';
+	numRounds += 1
+
+}
+
+function whosTheWinner() {
+	if (secondsLeft.innerText == "0") {
+		return 2;
+	}
+	if (player1CardVal > 21) {
+		if (player2CardVal > 21) {
+			return 0;
+		}
+		else {
+			return 2;
+		}
+	}
+	else if (player2CardVal > 21) {
+		return 1;
+	}
+	else if (player1CardVal > player2CardVal) {
+		return 1;
+	}
+	else if (player2CardVal > player1CardVal) {
+		return 2;
+	}
+	else {
+		if (player1Cards.length > player2CardVal.length) {
+			return 1;
+		}
+		else if (player2Cards.length > player1CardVal.length) {
+			return 2;
+		}
+		else {
+			return 0;
+		}
+	}	
+}
+
+function endGame() {
+	if (noPlayButton.innerText == "Go home") {
+		window.location.href = "start_blackjack.html";
+	}
+	initValues()
+	txtbox.style.display = "inline"
+	var scores = " You - " + P1Wins + " Player 2 - " + P2Wins;
+	if (P1Wins > P2Wins) {
+		txtbox.innerText = "Congrats! You won. You earned " + P1Wins + " points";
+	}
+	else if (P2Wins > P1Wins) {
+		txtbox.innerText = "You lost. " + scores;
+	}
+	else {
+		txtbox.innerText = "There was a tie." + scores;
+	}
+	playAgain.innerText = "Play again";
+	noPlayButton.innerText = "Go home";
+	playAgain.style.display = "inline";
+	noPlayButton.style.display = "inline";
+}
+
+function displayCurrScore() {
+
+	cards_to_display = cardCont2.getElementsByTagName("img");
+	for (let i = 0; i < cards_to_display.length; i++) {
+		cardSrc = String(player2Cards[i]).split(" ")[0] + String(player2Cards[i]).split(" ")[1]
+		cards_to_display[i].src = "cards/" + cardSrc + ".png"
+		cards_to_display[i].style.animation = "fadeIn 1s";
+	}
+	console.log('P1 ' + String(player1CardVal));
+	console.log('P2 ' + String(player2CardVal));
+}
+
+
+function displayP1Cards() {
+	var card1Src = String(player1Cards[0]).split(' ')[0] + String(player1Cards[0]).split(' ')[1];
+	var card1 = document.createElement('img');
+	card1.src = "cards/" + card1Src + ".png";
+	card1.style.width = "100px";
+	card1.style.marginRight = "25px"
+	cardCont1.appendChild(card1);
+
+	var card2Src = String(player1Cards[1]).split(" ")[0] + String(player1Cards[1]).split(" ")[1];
+	var card2 = document.createElement('img');
+	card2.src = "cards/" + card2Src + ".png";
+	card2.style.width = "100px";
+	card2.style.marginRight = "25px"
+	cardCont1.appendChild(card2);
+
+	addCardVal(1, player1Cards[0]);
+	addCardVal(1, player1Cards[1]);
+
+}
+
+function addCardVal(player, card) {
+	cardAmt = String(card).split(" ")[0]
+	console.log(cardAmt)
+	cardVal = 0;
+	if (cardAmt == "A") {
+		cardVal += 1;
+	}
+	else if (cardAmt == "Q" || cardAmt == "K" || cardAmt == "J") {
+		cardVal += 10;
+	}
+	else {
+		cardVal += Number(cardAmt)
+	}
+	if (player == 1) {
+		player1CardVal += Number(cardVal);
+	}
+	else if (player == 2) {
+		player2CardVal += Number(cardVal);
+	}
+}
+
+function isNumber(obj) {
+    return typeof obj === 'number';
+}
+
+function displayP2Cards() {
+	var card1 = document.createElement('img');
+	card1.src = "playing-card.png";
+	card1.style.width = "100px";
+	card1.style.marginRight = "25px";
+	card1.style.borderRadius = "15px";
+	cardCont2.appendChild(card1);
+
+	var card2 = document.createElement('img');
+	card2.src = "playing-card.png";
+	card2.style.width = "100px";
+	card2.style.marginRight = "20px";
+	card2.style.borderRadius = "15px";
+	cardCont2.appendChild(card2);
+
+	addCardVal(2, player2Cards[0]);
+	addCardVal(2, player2Cards[1]);
+
 }
 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const crypto = require('crypto');
 const app = express();
 app.use(parser.json());
-const port = 80;
+const port = 3000;
 const path = require('path');
 
 const db = mongoose.connection;
@@ -16,25 +16,21 @@ db.on('error', console.error.bind(console, "MongoDB connection error"));
 
 app.use(express.static('public_html'));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public_html/start_blackjack.html'));
-});
-
-
 var Schema = mongoose.Schema;
 
-// var UserSchema = new Schema({
-//     username: String,
-//     password: String,
-//     level: Number,
-//     coins: Number
-//   });
+var UserSchema = new Schema({
+    username: String,
+    password: String,
+    totalPoints: Number,
+    friends: [],
+    gamePoints: []
+  });
   
-// var User = mongoose.model("User", UserSchema);
+var User = mongoose.model("User", UserSchema);
 
 // creates quick link to local host port
 app.listen(port, () => 
-  console.log(`App listening at http://localhost:${port}`))
+  console.log(`App listening at http://localhost:${3000}`))
 
 
 
