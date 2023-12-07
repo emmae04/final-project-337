026a23952303fb55ba140736000ea87b73f81ddc
NoelleHS <nhealeystewart@arizona.edu>
2023-12-06 21:48:00 -0700

    leaderboard

--- Main/public_html/app/leaderboard.js
+++ Main/public_html/app/leaderboard.js
@@ -68,3 +68,109 @@ function boggleNum(){
     }).catch((err) => { console.log(err) });
 
 }
+function TTTHighest(){
+    fetch( '/TTT/highestScores/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "Tic Tac Toe Highest Score Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+function TTTNum(){
+    fetch( '/TTT/numPlays/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "Tic Tac Toe Most Games Played Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+
+function TTTWs(){
+    fetch( '/TTT/winstreak/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "Tic Tac Toe Current Win Streaks Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+
+function TTTScore(){
+    fetch( '/TTT/scoreLeader/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "Tic Tac Toe Current Scores Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+
+function HangmanCurrent(){
+    fetch( '/Hangman/winstreak/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "Hangman Current Win Streaks Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+
+
+function HangmanPlays(){
+    fetch( '/Hangman/plays/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "Hangman Most Games Played Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+
+
+function HangmanWins()
+{
+    fetch( '/Hangman/wins/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "Hangman Most Games Won Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+
+
+function bjHighest()
+{
+    fetch( '/bj/highest/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "BlackJack Highest Score Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+
+function bjNum()
+{
+    fetch( '/bj/plays/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "BlackJack Most Games Played Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+
+function bjWins()
+{
+    fetch( '/bj/ws/', {
+    }).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById("result").innerText = text;
+        document.getElementById("resultLabel").innerText = "BlackJack Current Win Streaks Leaderboard"
+    }).catch((err) => { console.log(err) });
+}
+
